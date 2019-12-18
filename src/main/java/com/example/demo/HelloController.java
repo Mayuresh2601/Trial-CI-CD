9940d556d0acae5ca26488db08564f7ed59c7430
Mayuresh2601 <mssonar26@gmail.com>
2019-12-18 13:28:53 +0530

    Message Modified

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@GetMapping
 	public String hello() {
-		return "Hello World from Mayuresh Sonar to Bridgelabz Comp.(Gowandi West)";
+		return "Hello World from Mayuresh Sonar to Bridgelabz Company(Gowandi West)";
 	}
 	
 	@GetMapping("/register")
